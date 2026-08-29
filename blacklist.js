4b093a04951500df00fee97a52038f65784f7447
evagtw-ai <eva.gtw@gmail.com>
2026-08-29 21:56:44 +0800

    Create blacklist.js

---
+++ blacklist.js
@@ -0,0 +1,10 @@
+// 🛑 拼寫黑名單
+// 只要在這裡面的單詞，拼寫遊戲就會自動跳過它。
+// 你可以直接從 wordlist.js 複製整行貼過來！
+
+const BANNED_SPELL_WORDS = [
+    { cn: "火龍果", en: "dragon fruit" },
+    { cn: "熱情果", en: "passion fruit" }
+    // ⬇️ 可以在下面繼續貼上其他不想拼寫的單詞
+    
+];
