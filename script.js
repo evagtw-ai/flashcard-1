03242a56059e25ef8bd18add06ea731e897494ad
evagtw-ai <eva.gtw@gmail.com>
2026-09-08 00:04:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -339,7 +339,8 @@ function initCategory() {
     const wrap = document.getElementById("categoryWrap");
     if (!wrap) return;
     wrap.innerHTML = "";
-    const openCats = ["Occupation", "Place", "Color", "Animal", "Transportation", "Body", "Object", "Nature", "Food",  "Time", "Verb", "Clothing", "Objet", "All"];
+    const openCats = ["Occupation", "Place", "Color", "Nature", "Clothing", "Object", "Verb", "Animal", "Transportation", "Time", "Food", "Body", "People", "All"];
+
     Object.keys(wordData).forEach(key => {
         const btn = document.createElement("button");
         btn.innerHTML = `        <div style="font-size:22px; font-weight:bold;">${catNameMap[key]}</div>         <div style="font-size:14px; opacity:0.8;">${key}</div>        `;
